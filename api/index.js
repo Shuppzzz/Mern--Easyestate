10c5927a4d7d54d5af45124d35bba948ab307cc6
Akcodes <111561333+Shuppzzz@users.noreply.github.com>
2024-03-12 03:48:53 +0530

    create user update api

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import authRouter from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO)
@@ -14,6 +15,8 @@ mongoose.connect(process.env.MONGO)
 const app= express();
 
 app.use(express.json());
+
+app.use(cookieParser());
 
 app.listen(3000, ()=>{
     console.log('Server is running on port 3000');}
